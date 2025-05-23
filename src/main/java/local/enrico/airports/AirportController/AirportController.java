3b43c8103f6ac41ecf050979674d44a1c1e312c5
enrico <enricomendrot23@gmail.com>
2025-05-23 17:13:08 -0300

    Término da Fase 6 -Add classe AirportNearMeDTO -Add interface AirportNearMeProjection -Update interface AirportRepository  -add query personalizada  -add assinatura findNearMe; -Update classe AirportService  -add findNearMe -Update classe AirportController  -add endpoint /nearme com findNearMe

--- src/main/java/local/enrico/airports/AirportController/AirportController.java
+++ src/main/java/local/enrico/airports/AirportController/AirportController.java
@@ -1,10 +1,15 @@
 package local.enrico.airports.AirportController;
 
 import java.util.List;
+import local.enrico.airports.DTO.AirportMinDTO;
+import local.enrico.airports.DTO.AirportNearMeDTO;
 import local.enrico.airports.entities.Airport;
 import local.enrico.airports.repositories.AirportService.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,4 +28,51 @@ public class AirportController {
         List<Airport> result = airportService.findAll();
         return result;
     }
+    
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()) {
+               return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+    
+    @GetMapping("/country/{countryName}")
+    public ResponseEntity<List<AirportMinDTO>> findByCountryIgnoreCase(@PathVariable String countryName) {
+        
+        List<AirportMinDTO> result = airportService.findByCountry(countryName);
+        if (result.isEmpty()) {
+               return ResponseEntity.notFound().build(); 
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+    @GetMapping("/iatacode/{iataCode}")
+    public ResponseEntity<Airport> findByIataCode(@PathVariable String iataCode) {
+        Airport result = airportService.findByIataCode(iataCode);
+        
+        if (result == null) {
+               return ResponseEntity.notFound().build(); 
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+    
+    @GetMapping("/nearme")
+    public ResponseEntity<List<AirportNearMeDTO>> findNearMe (
+            @RequestParam double latitude,
+            @RequestParam double longitude
+    ) {
+        List<AirportNearMeDTO> result = airportService.findNearMe(latitude, longitude);
+        
+        if (result.isEmpty()) {
+               return ResponseEntity.notFound().build(); 
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+    
 }
--- src/main/java/local/enrico/airports/repositories/AirportService/AirportService.java
+++ src/main/java/local/enrico/airports/repositories/AirportService/AirportService.java
@@ -1,11 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package local.enrico.airports.repositories.AirportService;
 
 import java.util.List;
+import local.enrico.airports.DTO.AirportMinDTO;
+import local.enrico.airports.DTO.AirportNearMeDTO;
 import local.enrico.airports.entities.Airport;
+import local.enrico.airports.projections.AirportNearMeProjection;
 import local.enrico.airports.repositories.AirportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +25,38 @@ public class AirportService {
             List<Airport> result = airportRepository.findAll();
             return result;
             
+            
         }
 
+        public List<Airport> findByCity(String city) {
+            List<Airport> result = airportRepository.findByCityIgnoreCase(city);
+            return result;
+        }
+        
+        
+    public List<AirportMinDTO> findByCountry(String country) {
+        List<Airport> resultAirport = airportRepository.findByCountryIgnoreCase(country);
+        
+        List<AirportMinDTO> resultDTO = resultAirport.stream()
+                .map(x -> new AirportMinDTO(x)).toList();
 
+        return resultDTO;
+        
+    }
+    
+    public Airport findByIataCode(String iataCode) {
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+
+    }
+    
+    public List<AirportNearMeDTO> findNearMe (double latitude, double longitude) {
+        List<AirportNearMeProjection> resultNearAirports = airportRepository.findNearMe(latitude, longitude);
+        
+        List<AirportNearMeDTO> resultDTO = resultNearAirports.stream()
+                .map(x -> new AirportNearMeDTO(x)).toList();
+        
+        return resultDTO;
+              
+    }
 }
